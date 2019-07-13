e74f0461b923fa6bd2241661d43b6d3390cceecc
Daniel Martin <danielmartin1205@gmail.com>
2019-07-12 22:57:45 -0400

    added styles

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -6,14 +6,15 @@ import { Link } from 'react-router-dom';
 export default class Dashboard extends Component {
     render() {
         return (
-            <aside className="nav-outer">
+            <header className="nav-outer">
+                <h1>Daniel Martin</h1>
                 <nav className="nav-container">
                     <Link to='/' className="link">Home</Link>
                     <Link to='/Projects' className="link">Projects</Link>
                     <Link to='/Resume' className="link">Resume</Link>
                     <Link to='/Contact' className="link">Contact</Link>
                 </nav>
-            </aside>
+            </header>
         )
     }
 }
