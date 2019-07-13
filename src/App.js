9297f466d44f9065e074536894b6af1f79a22add
Daniel Martin <danielmartin1205@gmail.com>
2019-07-13 00:38:24 -0400

    added contact and resume starter page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Route } from 'react-router-dom';
 import Dashboard from './Components/Dashboard';
 import Projects from './Components/Projects';
 import Home from './Components/Home';
+import Contact from './Components/Contact';
+import Resume from './Components/Resume';
 
 export default class App extends Component {
   render() {
@@ -12,6 +14,8 @@ export default class App extends Component {
         <Dashboard />
         <Route exact path= '/' component={Home} />
         <Route path='/Projects' component={Projects} />
+        <Route path='/Resume' component={Resume} />
+        <Route path='/Contact' component={Contact} />
       </div>
     )
   }
---
+++ src/Components/Contact.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+export default class Contact extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <form>
+                <input type="text"></input>
+                <input type="text"></input>
+                <input type="text"></input>
+                <input type="text"></input>
+            </form>
+        )
+    }
+}
